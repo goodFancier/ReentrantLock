d8ba44891d94683da3b36e49bee08768f08e752a
Nikitin_AA <nikitin_aa@businessculture.ru>
2020-08-14 08:35:51 +0400

    Added deadlock situation

--- src/App.java
+++ src/App.java
@@ -15,15 +15,78 @@
  * limitations under the License.
  */
 
+import java.util.LinkedHashMap;
+
 public class App
 {
+		/**
+		 * 1. 10 20
+		 * 2. 20 10
+		 * <p>
+		 * <p>
+		 * 1. 10 10
+		 * 2. 20 20
+		 * 3. 10 20
+		 * 4. 20 10
+		 */
+		private void deadLockMethod()
+		{
+				ResourceClass1 resourceClass1 = new ResourceClass1();
+				ResourceClass1 resourceClass2 = new ResourceClass1();
+				Thread thread = new Thread(() -> {
+						synchronized(resourceClass1)
+						{
+								resourceClass1.setA(10);
+								try
+								{
+										Thread.sleep(1000);
+								}
+								catch(InterruptedException e)
+								{
+										e.printStackTrace();
+								}
+								synchronized(resourceClass2)
+								{
+										resourceClass2.setA(30);
+								}
+								System.out.println(resourceClass1.getA());
+								System.out.println(resourceClass2.getA());
+						}
+				});
+				thread.setDaemon(false);
+				thread.start();
+				Thread thread2 = new Thread(() -> {
+						synchronized(resourceClass2)
+						{
+								resourceClass2.setA(40);
+								try
+								{
+										Thread.sleep(1000);
+								}
+								catch(InterruptedException e)
+								{
+										e.printStackTrace();
+								}
+								synchronized(resourceClass1)
+								{
+										resourceClass1.setA(20);
+								}
+								System.out.println(resourceClass2.getA());
+								System.out.println(resourceClass1.getA());
+						}
+				});
+				thread2.setDaemon(false);
+				thread2.start();
+		}
+
 		public static void main(String[] args)
 		{
 				/*ThreadSafeWithReentrantReadWriteLock threadSafeWithReentrantReadWriteLock = new ThreadSafeWithReentrantReadWriteLock();
 				threadSafeWithReentrantReadWriteLock.startThreadProcess();*/
-				ThreadSafeWithReentrantLock threadSafeWithReentrantLock = new ThreadSafeWithReentrantLock();
-				threadSafeWithReentrantLock.startThreadProcess();
-				/*ThreadSafeWithSynchronization threadSafeWithSynchronization = new ThreadSafeWithSynchronization();
+				/*ThreadSafeWithReentrantLock threadSafeWithReentrantLock = new ThreadSafeWithReentrantLock();
+				threadSafeWithReentrantLock.startThreadProcess();*/
+				/*ThreadSafeWithReentrantLock threadSafeWithSynchronization = new ThreadSafeWithReentrantLock();
 				threadSafeWithSynchronization.startThreadProcess();*/
+				new App().deadLockMethod();
 		}
 }
